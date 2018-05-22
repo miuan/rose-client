f93ce0f51d615aff1198bc9603ea23902a708039
Milan Medlik <milan.medlik@gmail.com>
2018-05-22 18:30:14 +0200

    implement connection to graphcool

--- Services.js
+++ Services.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { graphql, ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 
-
-export default class Services extends React.Component {
+export class Services extends React.Component {
   constructor(...args){
     super(...args);
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{color:'#fff'}}>AHOJ</Text> 
-        
+      <View style={styles.container}>    
+        {this.props.data.loading ? <Text style={{color:'#fff'}}>Loading...</Text> : null}
+        {this.props.data.allServiceses ? this.props.data.allServiceses.map((s) => <Text style={{color:'#a1a1f1'}}>{s.text}</Text>) : null}
       </View>
     );
   }
 }
+
+export default graphql(gql`
+  {allServiceses{
+    id,
+    text,
+    title,
+    language
+  }}
+`)(Services);
 
 const styles = StyleSheet.create({
   container: {
